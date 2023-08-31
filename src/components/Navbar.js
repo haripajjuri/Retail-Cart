85dc94bc11f3c39136b654ef8e27e6436bfbbb31
haripajjuri <20mh1a4241@acoe.edu.in>
2023-09-01 02:17:06 +0530

    changes made to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,14 @@
 import React from "react";
 export default function Navbar(){
     return(
-        <div><p>e-commerce website</p></div>
+        <div className="navContainer">
+            <div id="logo"><p>logo</p></div>
+            <div id="navComponents">
+                <div className="navItems" ><p> Home </p></div>
+                <div className="navItems" ><p> About </p></div>
+                <div className="navItems" ><p> Contact </p></div>
+                <div className="navItems" ><p> Favourites </p></div>
+            </div>
+        </div>
     )
 }
