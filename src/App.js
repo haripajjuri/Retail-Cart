b7603dcf0e38c395eefdbdc486eeff44262d5210
haripajjuri <20mh1a4241@acoe.edu.in>
2023-09-01 10:35:38 +0530

    slider updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Slider/>
     </div>
   );
 }
