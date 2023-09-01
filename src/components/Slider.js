c8df232dc7c441b13a5bfa87a08efa8851f382e5
haripajjuri <20mh1a4241@acoe.edu.in>
2023-09-01 12:08:15 +0530

    built slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,22 @@
 import React from "react"
+import leftIcon from '../left-arrow.png'
+import rightIcon from '../right-arrow.png'
+import img from '../images/main_image_star-forming_region_carina_nircam_final-1280.jpg'
 export default function Slider(){
     return(
         <div className="SliderContainer">
+            {/* <div className="arrows">
+                <p className="arrow"><img src={leftIcon} alt="arrow" /></p>
+                <p className="arrow"><img src={rightIcon} alt="" /></p>
+            </div> */}
 
+            <div className="images">
+                <div className="slide">1</div>
+                <div className="slide">2</div>
+                <div className="slide">3</div>
+                <div className="slide">4</div>
+            </div>
+            
         </div>
     )
 }
